fa3bd49a65551a31b86be1a18e1861a7e13111df
Angello Astorga <angello.astorga@gmail.com>
2019-06-29 18:31:16 -0500

    adding precis file

--- Precis/precis.py
+++ Precis/precis.py
@@ -1,11 +1,17 @@
-#import 
+import os
+from Data.problem import Problem
 
 
 
 def learnPost():
+    sln = os.path.abspath('../ContractsSubjects/Stack/Stack.sln')
+    projectName =  'StackTest' 
+    testFileName = 'StackContractTest.cs' 
+    PUTs = ['PUT_PushContract', 'PUT_PopContract', 'PUT_PeekContract', 'PUT_CountContract', 'PUT_ContainsContract']
 
-    #p = Problem()
 
+    p = Problem(sln, projectName, testFileName, PUTs)
+    print ("here")
 
 
 
