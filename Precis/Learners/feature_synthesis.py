355aaa449e9862b716dd39ab1ab2c204853e7096
Angello Astorga <angello.astorga@gmail.com>
2019-07-05 00:25:42 -0500

    adding todo notes

---
+++ Precis/Learners/feature_synthesis.py
@@ -0,0 +1,28 @@
+from z3 import *
+from Data.precis_feature import PrecisFeature
+
+class FeatureSynthesis:
+
+    #def __init__(self):
+    def GenerateDerivedFeatures(self,baseFeatures):
+        intFeatures = [f for f in baseFeatures if str(f.varZ3.sort())=="Int"]
+        self.CreateEqualities(intFeatures)
+
+    def CreateEqualities(self, intFeatures):
+        print(intFeatures)
+        #create copy constructor for PrecisFeature
+        """
+        namesDataFile = list()
+        if len(intVars) >= 2:
+            all_combination = itertools.combinations(intVars, 2)
+            for (var1, var2) in all_combination:
+                namesExpr = "(= " + var1 + " " + var2 + ")"
+                dataExpr = "(" + var1 + " == " + var2 + ")"
+                namesDataFile.append((namesExpr, dataExpr))
+
+                namesExpr = "(not (= "  + var1 + " " + var2 + "))"
+                dataExpr = "(not ("  + var1 + " == " + var2 + "))"
+                namesDataFile.append((namesExpr, dataExpr))
+                
+        return namesDataFile
+        """
