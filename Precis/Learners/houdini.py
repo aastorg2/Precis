01f5a653325554daef4256b7cba9a77a81255c14
Angello Astorga <angello.astorga@gmail.com>
2019-07-08 14:49:32 -0500

    removing todo

--- Precis/Learners/houdini.py
+++ Precis/Learners/houdini.py
@@ -15,13 +15,10 @@ class Houdini:
         print(features)
 
         #TODO: f of type FeatureVector --> make this indexable; this line wont work
-        #print(features[i], f[i])
         for f in featureVectors:
             print(f)
             print (type(f))
             for i in  range(len(features)):
-                #print(features[i])
-                #print(f.valuesZ3[i]) # this will throw index out range
                 print(features[i], f[i])
 
         print(type(deriveFeatureVec))
