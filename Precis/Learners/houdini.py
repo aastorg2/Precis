6ffb7a24161425145716d9e77f8cc77f2bab612b
Angello Astorga <angello.astorga@gmail.com>
2019-07-10 00:47:05 -0500

    refactoring

--- Precis/Learners/houdini.py
+++ Precis/Learners/houdini.py
@@ -7,23 +7,23 @@ class Houdini:
     useBounds = False
 
 
-    def generateDerivedFeatureVectors(self, derivedFeatures, features, featureVectors):
+    def generateDerivedFeatureVectors(self, derivedFeatures, baseFeatures, featureVectors):
         deriveFeatureVec = ()
         print(derivedFeatures)
         print(featureVectors)
         print ("here")
-        print(features)
+        print(baseFeatures)
         
         
         for f in featureVectors:
             # consider removing check for perfomances in cases where the number of feature vectors gets large.
             # number of base features should be the same as the number of entries in feature vector(values of said features)
-            assert(len(f) == len(features))
-             
+            assert(len(f) == len(baseFeatures))
+
             print("feature vec: " +str(f))
             pairs = list()
-            for i in  range(len(features)):
-                pair = (features[i], f[i])
+            for i in  range(len(baseFeatures)):
+                pair = (baseFeatures[i], f[i])
                 pairs.append(pair)
             print(pairs)
         
@@ -31,6 +31,9 @@ class Houdini:
 
         #for df in derivedFeatures:
         #    print(df)
+
+    #def generateFeatureValueMapping()
+
 
     def learn(featureVector):
         pass
