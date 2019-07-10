20306ee05dfeff8a6829d1344eb5639e401dcb43
Angello Astorga <angello.astorga@gmail.com>
2019-07-10 02:53:38 -0500

    using z3 api substitute to create dervied values for feature vectors

--- Precis/Learners/houdini.py
+++ Precis/Learners/houdini.py
@@ -8,31 +8,38 @@ class Houdini:
 
 
     def generateDerivedFeatureVectors(self, derivedFeatures, baseFeatures, featureVectors):
-        deriveFeatureVec = ()
+        
         print(derivedFeatures)
         print(featureVectors)
         print ("here")
         print(baseFeatures)
-        
-        
+        pairs = list()
+        # consider
         for f in featureVectors:
-            # consider removing check for perfomances in cases where the number of feature vectors gets large.
-            # number of base features should be the same as the number of entries in feature vector(values of said features)
-            assert(len(f) == len(baseFeatures))
-
             print("feature vec: " +str(f))
-            pairs = list()
-            for i in  range(len(baseFeatures)):
-                pair = (baseFeatures[i], f[i])
-                pairs.append(pair)
+            pairs = Houdini.generateFeatureValueMapping(baseFeatures,f)
             print(pairs)
-        
-        print(type(deriveFeatureVec))
+            #print(type(pairs))
+            for df in derivedFeatures:
+                #TODO: the substitute does not work
+                subDf = substitute(simplify(df.varZ3) , pairs)
+                #print(subDf)
+                #print(type(df.varZ3))
 
         #for df in derivedFeatures:
         #    print(df)
 
-    #def generateFeatureValueMapping()
+    @staticmethod
+    def generateFeatureValueMapping(baseFeatures, featureVector):
+        pairs = list()
+        # consider removing check for perfomances in cases where the number of feature vectors gets large.
+        # number of base features should be the same as the number of entries in feature vector(values of said features)
+        assert(len(featureVector) == len(baseFeatures))
+        for i in  range(len(baseFeatures)):
+            #print("type of featVec", type(featureVector[i]))
+            pair = (baseFeatures[i].varZ3 , featureVector[i])
+            pairs.append(pair)
+        return pairs
 
 
     def learn(featureVector):
