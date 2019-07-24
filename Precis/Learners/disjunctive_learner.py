9e97a71d435b032fed147278a921267d314dfaa1
Angello Astorga <angello.astorga@gmail.com>
2019-07-24 18:22:31 -0500

    successfully removing feature and feature values in feature vector after choosing feature to split on

--- Precis/Learners/disjunctive_learner.py
+++ Precis/Learners/disjunctive_learner.py
@@ -7,7 +7,9 @@ from Learners.houdini import Houdini
 from os import sys
 import math
 import numpy
+import logging
 
+logger = logging.getLogger("Runner.DisjunctiveLearner")
 class DisjunctiveLearner:
 
     # entropy measure is used by default for choosing Predicates to split data on
@@ -19,13 +21,83 @@ class DisjunctiveLearner:
         if k == 0:
             return houdini.learn(features, featureVectors)
         else:
-            self.chooseFeature(features, featureVectors)
+            (f,idx, fposFv,fnegFv) = self.chooseFeature(features, featureVectors)
+            
+            #region
+            print("feature:", f)
+            print("index: ", idx)
+            print()
+            print(fposFv)
+            print()
+            print( [ fv[idx]  for fv in fposFv] )
+            print()
+            print(fnegFv)
+            print()
+            print( [ fv[idx]  for fv in fnegFv] )
+            #endregion
+            #region checking removal of feature
 
+            # print("before length: ",len(features))
+            # check = [ nf for nf in features if nf.varZ3 == f.varZ3] 
+            # print(check)
+            # features = features[0:idx]+ features[idx+1:]
+            # check = [ nf for nf in features if nf.varZ3 == f.varZ3] 
+            # print(check)
+            # print("length: ", len(features))
+            #endregion 
+            
+            #region checking removal of value of feature at index in feature vector
+            #print("before removal")
+            # fvLens = [ len(fv) for fv in featureVectors ]
+            # print(fvLens)
+            
+            # valueAtIdx  = [ fv[idx] for fv in featureVectors ]
+            # print("feature value before removing:")
+            # print(valueAtIdx)
+
+            #for fv in featureVectors:
+            #    fv.values = fv.values[0:idx] + fv.values[idx+1:]
+            #    fv.valuesZ3 = fv.valuesZ3[0:idx] + fv.valuesZ3[idx+1:]
+            # nextfvLens = [ len(fv.values) for fv in featureVectors ]
+            # print("after removal")
+            # print(nextfvLens)
+            
+            # valueAtIdx  = [ fv[idx] for fv in featureVectors ]
+            # print("feature value after removing:")
+            # print(valueAtIdx)
+            #nextFeatureVector = FeatureVector([], [], str(.testLabel))
+            #endregion
+            newFeatures = features[0:idx]+ features[idx+1:]
+
+            for fv in fposFv:
+                fv.values = fv.values[0:idx] + fv.values[idx+1:]
+                fv.valuesZ3 = fv.valuesZ3[0:idx] + fv.valuesZ3[idx+1:]
+            
+            for fv in fnegFv:
+                fv.values = fv.values[0:idx] + fv.values[idx+1:]
+                fv.valuesZ3 = fv.valuesZ3[0:idx] + fv.valuesZ3[idx+1:]
+
+            #print( [ len(fv)  for fv in fposFv] )
+            #print( [ len(fv)  for fv in fnegFv] )
+            #posPost = learn(k-1,newFeatures,fposFv)
+            #negPost = learn(k-1,newFeatures,fnegFv)
+
+            #print("positive: ",posPost.toInfix())
+            #print("negative: ",negPost.toInfix())
+
+            
+            #nextFeatureVector.values =  
+            #nextFeatureVector.valuesZ3 = self.valuesZ3 + derivedValuesZ3
+
+
+            
     #return feature along with index
     def chooseFeature(self, features, featureVectors):
         # TODO: figure is removing always false predicates will lead to optimizations
-        fvFPos = list()
-        fvFNeg = list()
+        fvPos = list()
+        fvNeg = list()
+        
+        allScores = []
         for idx in range(0, len(features)):
             (fvPos, fvNeg) = self.splitSamples(features[idx], idx, featureVectors) 
             #skip always false predicates
@@ -34,10 +106,15 @@ class DisjunctiveLearner:
             posLabel = ['+'] * len(fvPos)
             negLabel = ['-'] * len(fvNeg)
             score = self.entropy(posLabel+negLabel)
-            print("feature: ", features[idx])
-            print("entropy: ", score)
+            allScores.append({'predicate': features[idx],'idx': idx, 'score': score , 'posData':fvPos, 'negData': fvNeg} )
+            
         
-        sys.exit(0)
+        sortedScores = sorted(allScores, key=lambda x: x['score'])
+        #for entry in sortedScores:
+        #    logger.info("predicate: "+ str(entry['predicate']))
+        #    logger.info("predicate: "+ str(entry['score']))
+        #return highest entropy
+        return (sortedScores[-1]['predicate'], sortedScores[-1]['idx'], sortedScores[-1]['posData'], sortedScores[-1]['negData']) 
 
     # f is for feature of PrecisFeature type
     def splitSamples(self, f, idx, featureVectors):
@@ -70,3 +147,9 @@ class DisjunctiveLearner:
         return - (probability_value_attribute * numpy.log(probability_value_attribute) / numpy.log(base)).sum()
         # Note: I believe this implementation is missing an additional multiplication
         #return - ((probability_value_attribute/initialTotalSample)* probability_value_attribute * numpy.log(probability_value_attribute) / numpy.log(base)).sum()
+
+if __name__ == '__main__':
+
+    #PrecisFeature: def __init__(self, isDerived, varName, varType=None, isNew=None, varZ3=None):
+
+    feature1 = PrecisFeature(False, "New_Containsx", "BOOL", "New_Containsx".startswith("New_"), None)
