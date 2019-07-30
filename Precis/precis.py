1286e32844db0a1a1006ec4811ed0aa93e07ec88
Angello Astorga <angello.astorga@gmail.com>
2019-07-30 15:24:05 -0500

    refactoring the passing of arguments to learnPost function in precis main

--- Precis/precis.py
+++ Precis/precis.py
@@ -12,7 +12,8 @@ import command_runner
 import logging
 
 
-def learnPost():
+def learnPost(p,PUTName, outputFile):
+
     logger = logging.getLogger("Runner")
     logger.setLevel(logging.INFO)
     # create the logging file handler
@@ -23,18 +24,7 @@ def learnPost():
     logger.addHandler(fh)
 
 
-    sln = os.path.abspath('../ContractsSubjects/Stack/Stack.sln')
-    projectName =  'StackTest'
-    testDebugFolder = '../ContractsSubjects/Stack/StackTest/bin/Debug/'
-    testDll = testDebugFolder + 'StackTest.dll'
-    testFileName = 'StackContractTest.cs' 
-    testNamepace = 'Stack.Test'
-    testClass = 'StackContractTest'
-    PUTs = ['PUT_PushContract', 'PUT_PopContract', 'PUT_PeekContract', 'PUT_CountContract', 'PUT_ContainsContract'] 
-    PUTName = 'PUT_PushContract'
-    outputFile = os.path.abspath('./typesOM.txt')
-
-    p = Problem(sln, projectName, testDebugFolder, testDll, testFileName, testNamepace, testClass)
+    
     p.ExtractObservers(PUTName, outputFile)
 
     #returns list of base features
@@ -85,14 +75,18 @@ def learnPost():
         #print(derivedFeatures)
         #print(featureVectors)
 
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    learnPost()
+    sln = os.path.abspath('../ContractsSubjects/Stack/Stack.sln')
+    projectName =  'StackTest'
+    testDebugFolder = '../ContractsSubjects/Stack/StackTest/bin/Debug/'
+    testDll = testDebugFolder + 'StackTest.dll'
+    testFileName = 'StackContractTest.cs' 
+    testNamepace = 'Stack.Test'
+    testClass = 'StackContractTest'
+    PUTs = ['PUT_PushContract', 'PUT_PopContract', 'PUT_PeekContract', 'PUT_CountContract', 'PUT_ContainsContract'] 
+    PUTName = 'PUT_PushContract'
+    outputFile = os.path.abspath('./typesOM.txt')
+
+    p = Problem(sln, projectName, testDebugFolder, testDll, testFileName, testNamepace, testClass)
+    
+    learnPost(p, PUTName, outputFile)
