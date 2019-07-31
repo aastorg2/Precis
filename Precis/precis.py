c1c114819cc36d0ca5070cd7c7641d74a7ddc4b8
Angello Astorga <angello.astorga@gmail.com>
2019-07-31 13:31:08 -0500

    added wrapper learning function

--- Precis/precis.py
+++ Precis/precis.py
@@ -7,13 +7,14 @@ from Learners.feature_synthesis import FeatureSynthesis
 from Learners.houdini import Houdini
 from Learners.disjunctive_learner import DisjunctiveLearner
 from Teachers.instrumenter import Instrumenter
+from Data.precis_formula import PrecisFormula
 import command_runner
 
 import logging
 
 
 def learnPost(p,PUTName, outputFile):
-
+    
     logger = logging.getLogger("Runner")
     logger.setLevel(logging.INFO)
     # create the logging file handler
@@ -22,15 +23,40 @@ def learnPost(p,PUTName, outputFile):
     fh.setFormatter(formatter)
     # add handler to logger object
     logger.addHandler(fh)
+    #todo: test for k==2
+    #post = learnPostUpToK(p,PUTName,outputFile,0)
+    postK1 = learnPostUpToK(p,PUTName, outputFile,1)
+    #postK2 = learnPostUpToK(p,PUTName, outputFile,2)
+    #print("smallest post up to k == 0", post.toInfix())
+    print("smallest post up k == 1",postK1.toInfix())
+    #print("smallest post up k == 2",postK2.toInfix())
+    """
+    implication = Implies(post.formulaZ3,postK1.formulaZ3)
+    solver0 = Solver()
+    solver0.add(Not(implication)) # check (not (post => post1)) is unsat
+    check0 = solver0.check()
+    logger.info("first check\n")
+    logger.info(solver0.to_smt2())
+    print("is it unsat?", check0 ) # if unsat, stop
+    otherside = Implies(postK1.formulaZ3,postK2.formulaZ3)
+    solver1 = Solver()
+    solver1.add(Not(otherside))
+    check1 = solver1.check()
+    print("is it unsat?", check1 )
+    """
+def learnPostUpToK(p,PUTName, outputFile, k):
 
-
-    
     p.ExtractObservers(PUTName, outputFile)
 
     #returns list of base features
     baseFeatures = p.ReadObserversFromFile(outputFile)
     allPostconditions = list()
     allBaseFeatureVectors =[]
+    
+    inst = Instrumenter("MSBuild.exe","./Instrumenter/Instrumenter/bin/Debug/Instrumenter.exe")
+    initFormula = PrecisFormula(BoolVal(False))
+    inst.instrumentPost(p,initFormula , PUTName)
+
     while True:
         pex = Pex()
         
@@ -56,9 +82,13 @@ def learnPost(p,PUTName, outputFile):
         
         postcondition = None
         disLearner = DisjunctiveLearner()
-        postcondition = disLearner.learn(0,boolFeatures, boolFeatureVectors)
+        postcondition = disLearner.learn(k,boolFeatures, boolFeatureVectors)
         #sys.exit(0)
-        print("before to infix")
+        print("postcondition formulaZ3")
+        print(postcondition.formulaZ3)
+        print("postcondition formula")
+        print(postcondition.formula)
+        print("postcondition infix")
         print(postcondition.toInfix())
         
         # assumes ms build in path
@@ -67,7 +97,7 @@ def learnPost(p,PUTName, outputFile):
 
         if postcondition.formula in allPostconditions:
             print("found it")
-            break
+            return postcondition
         
         allPostconditions.append(postcondition.formula)
         #assert(bfv[0] <--> dfv[0] )
